9f8a107bebe7a4096a0e3832c683707ddded82e9
Thành Lã <thanhlh@lifetimetech.vn>
2018-11-27 15:34:55 +0700

    Initial Commit

---
+++ AudioVisualizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioVisualizer
+//
+//  Created by Thành Lã on 11/27/18.
+//  Copyright © 2018 MonstarLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
