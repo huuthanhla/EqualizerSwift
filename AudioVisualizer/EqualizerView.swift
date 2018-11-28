a0d7089506c8827aea35b6f383d5bdb676709e0a
Thành Lã <thanhlh@lifetimetech.vn>
2018-11-28 09:34:10 +0700

    Override draw rect with scale

--- AudioVisualizer/EqualizerView.swift
+++ AudioVisualizer/EqualizerView.swift
@@ -10,15 +10,61 @@ import UIKit
 import DisPlayers_Audio_Visualizers
 import EZAudioiOS
 
-class EqualizerView: DPRollingEqualizerView {
+protocol EqualizerViewOptions {
+    var scale: CGFloat { get }
+}
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+extension EqualizerViewOptions {
+    var scale: CGFloat {
+        return 0.75
     }
-    */
+}
+
+class EqualizerView: DPRollingEqualizerView, EqualizerViewOptions {
+
+    override func draw(_ rect: CGRect) {
+        
+        let ctx = UIGraphicsGetCurrentContext()
+        ctx?.saveGState()
+        let frame: CGRect = bounds
+        
+        backgroundColor?.set()
+        UIRectFill(frame)
+        
+        let columnWidth: CGFloat = (rect.size.width * scale) / (CGFloat(equalizerSettings.numOfBins) - 1)
+        
+        let actualWidth = max(1, columnWidth * (1 - 2 * equalizerSettings.padding))
+        let actualPadding = max(0, (columnWidth - actualWidth) / 2)
+        
+        guard let timeHeights = audioService()?.timeHeights() as? [CGFloat] else { return }
+        for i in 0 ..< Int(equalizerSettings.numOfBins) {
+            let columnHeight = timeHeights[i] / 2
+            
+            if columnHeight <= 0 {
+                continue
+            }
+            let columnX: CGFloat = CGFloat(i) * columnWidth
+            
+            var rollingPath = UIBezierPath()
+            rollingPath = UIBezierPath(roundedRect: CGRect(x: columnX + actualPadding, y: frame.height / 2 - columnHeight / 2, width: actualWidth, height: columnHeight), cornerRadius: actualWidth)
+            
+            equalizerBinColor.setFill()
+            
+            rollingPath.fill()
+        }
+        
+        equalizerBinColor.setStroke()
+        
+        let linePath = UIBezierPath()
+        
+        linePath.lineWidth = 2.0
+        linePath.move(to: CGPoint(x: rect.width * scale + actualPadding, y: rect.height / 2))
+        linePath.addLine(to: CGPoint(x: rect.width, y: rect.height / 2))
+        linePath.stroke()
+        
+        ctx?.restoreGState()
+    }
+
 
 }
 
